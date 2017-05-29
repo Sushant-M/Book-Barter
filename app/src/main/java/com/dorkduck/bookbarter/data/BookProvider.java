ed8c259b3f8157357d85a86102790811dce58803
Sushant Mathur <sushant.post@gmail.com>
2017-05-29 18:31:00 +0530

    Added stage 1 contentprovider code

--- app/src/main/java/com/dorkduck/bookbarter/data/BookProvider.java
+++ app/src/main/java/com/dorkduck/bookbarter/data/BookProvider.java
@@ -1,8 +1,12 @@
 package com.dorkduck.bookbarter.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,8 +16,25 @@ import android.support.annotation.Nullable;
  */
 
 public class BookProvider extends ContentProvider {
+
+    static final String AUTHORITY = "com.dorkduck.bookbarter";
+    static final String URL = "content://" + AUTHORITY + "/books";
+    static final Uri uri = Uri.parse(URL);
+
+    private SQLiteDatabase sqLiteDatabase;
+
+    public DatabaseHelper databaseHelper;
+
+    public static UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static{
+        uriMatcher.addURI(AUTHORITY,"books",0);
+    }
+
+
     @Override
     public boolean onCreate() {
+        databaseHelper = new DatabaseHelper(getContext());
         return false;
     }
 
@@ -32,7 +53,17 @@ public class BookProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+
+        sqLiteDatabase = databaseHelper.getWritableDatabase();
+
+        long rowid = sqLiteDatabase.insert(Contract.Entries.TABLE_NAME,"",values);
+
+        if(rowid>0){
+            Uri uri_ = ContentUris.withAppendedId(uri,rowid);
+            getContext().getContentResolver().notifyChange(uri_,null);
+            return uri_;
+        }
+        throw new SQLException("Couldn't insert into database"+ uri);
     }
 
     @Override
